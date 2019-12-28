1aab29466b2e8164f86cf8fd28d62806baffc50c
Emma-90844 <55737744+Emma-90844@users.noreply.github.com>
2019-12-28 11:03:43 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,7 +1,19 @@
+/*A random number guessing game that generates a number between 1 and 6 and gives thr player 
+two attempts to guess the number correctly*/
 
+//Assume the player didnt guess correctly
 var correctGuess = false;
+
+//Generate a random number 1 to 6
 var randomNumber = Math.floor(Math.random() * 6 ) + 1;
+
 var guess = prompt("Am thinking of a number between 1 and 6,What is that number?")
+
+/*Test and see if a player is:
+1-correct
+2-guessed too high
+3-guessed too low*/
+
 if(parseInt(guess) === randomNumber){
     correctGuess = true; 
 
@@ -15,6 +27,7 @@ if(parseInt(guess) === randomNumber){
     if(parseInt(guessLess) === randomNumber){
         correctGuess = true;
     }
+//Test and see if a player is correct and respond
 } if(correctGuess ){
     document.write("You guessed correctly")
 } else {

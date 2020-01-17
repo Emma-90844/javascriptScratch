6506b3eba94b7b98d304ffb1936fa95332a53692
Emma-90844 <55737744+Emma-90844@users.noreply.github.com>
2020-01-17 10:01:42 +0300

    Update random.js

--- random.js
+++ random.js
@@ -1,18 +1,19 @@
-// var firstValue = prompt("Enter your first value");
-// var secondValue = prompt("Enter your second value");
-// var fValue = parseInt(firstValue)
-// var sValue  =parseInt(secondValue)
-// var finalValue = Math.floor(Math.random() * sValue) + fValue;
-// alert("your random number is " + finalValue) 
+/*
+var firstValue = prompt("Enter your first value");
+var secondValue = prompt("Enter your second value");
+var fValue = parseInt(firstValue)
+var sValue  =parseInt(secondValue)
+var finalValue = Math.floor(Math.random() * sValue) + fValue;
+alert("your random number is " + finalValue) 
 
 
 
-// var input = prompt("Please type in number");
-// var topNumber = parseInt(input);
-// var input1 = prompt("Please type anather number")
-// var bottomNumber  = parseInt(input1)
-// var randomNumber = Math.floor(Math.random() * topNumber) + bottomNumber;
-// document.write(randomNumber + " is a random number between " + topNumber + " and " + bottomNumber)
+var input = prompt("Please type in number");
+var topNumber = parseInt(input);
+var input1 = prompt("Please type anather number")
+var bottomNumber  = parseInt(input1)
+var randomNumber = Math.floor(Math.random() * topNumber) + bottomNumber;
+document.write(randomNumber + " is a random number between " + topNumber + " and " + bottomNumber)*/
 
 var input1 = prompt("Please type a starting  number");
 var buttomNumber = parseInt(input1);
@@ -21,7 +22,7 @@ var topNumber  = parseInt(input)
 var randomNumber = Math.floor(Math.random() * (topNumber-bottomNumber + 1)) + 1;
 document.write(randomNumber + " is a random number between " + topNumber + " and " + bottomNumber)
 
-//Conditional statements redefines what we want how program to behave like
+//Conditional statements redefines what we want our program to behave like.
  
 
 
